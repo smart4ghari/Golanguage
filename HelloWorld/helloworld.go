6c67dce7ceb63a152b6258c6d58d6e60dc626bff
Hariharan M <hariharan11042001@gmail.com>
2020-06-15 20:52:16 +0530

    HelloWorld Program in Go

---
+++ HelloWorld/helloworld.go
@@ -0,0 +1,11 @@
+//Here you should always start with package main
+package main
+
+// The second most important keyword is import fmt
+import "fmt"
+
+//You should also define the function
+func main() {
+
+	fmt.Println("Hello, world!")
+}
